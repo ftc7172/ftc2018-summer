fb1563855113d507a972cb6cff792609c99694d4
MBCallahan <mbc042000@gmail.com>
2018-05-07 14:29:19 -0500

    fixed PID class to no longer use the time factor which made it harder to get the gains right. PID class now uses shifting window accumulator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.ArrayList;
+
 /**
  * Created by Owner on 9/4/2017.
  */
 
 public class PID {
+    final short BUFFER_SIZE=5;
     double kP;
     double kI;
     double kD;
@@ -12,24 +15,26 @@ public class PID {
     private double lastError;
 
     private double acc;
-    private double lastT;
 
-    public PID(double K, double I, double D) {
-        kP = K;
+    private ArrayList<Double> buffer;
+
+
+    public PID(double p, double I, double D) {
+        kP =p;
         kI = I;
         kD = D;
         reset();
     }
 
-    public PID(double K, double D) {
-        kP = K;
+    public PID(double p, double D) {
+        kP = p;
         kI = 0;
         kD = D;
         reset();
     }
 
-    public PID(double K) {
-        kP = K;
+    public PID(double p) {
+        kP = p;
         kI = 0;
         kD = 0;
         reset();
@@ -37,37 +42,39 @@ public class PID {
 
     public void reset() {
         acc = 0;
-        lastT = 0;
         lastError = 0;
-    }
-    public void reset(double t) {
-        acc = 0;
-        lastT = t;
-        lastError = 0;
+        for(short i = 0; i<BUFFER_SIZE; i++) buffer.add(0.0);
     }
 
 
-    public double calc(double pos, double setPoint, double t) {
+
+    public double calc(double pos, double setPoint) {
 
         double error = setPoint - pos;
         double p = kP * error;
-        if (lastT == 0)
-            lastT = t;
-        double dT = 1;//t - lastT;
-        acc += trapazoidInt(error, dT);
+
+
+        acc=accumulatorBuffer(error);
         double i = kI * acc;
-        double d = (error - lastError) / dT;
+        double d = (error - lastError);
         d *= kD;
 
         lastError = error;
-        lastT = t;
+
         return p + i + d;
 
-
+    }
+    private double accumulatorBuffer(double x){
+        buffer.remove(0);
+        buffer.add(x);
+        double sum=0;
+        for(double i: buffer){
+            sum+=i;
+        }
+        return sum;
     }
 
-    private double trapazoidInt(double error, double dT) {
-        return dT * (lastError + error) / 2;
-    }
+
+
 
 }
